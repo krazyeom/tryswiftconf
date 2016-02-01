1b4b5dade69d4a4967f33ff150a6495607ca8cfd
Thi <t@thi.im>
2016-02-01 15:22:18 +0900

    Initial Commit

---
+++ tryswiftconf/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tryswiftconf
+//
+//  Created by Doan Truong Thi on 2/1/16.
+//
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tryswiftconfTests/tryswiftconfTests.swift
@@ -0,0 +1,36 @@
+//
+//  tryswiftconfTests.swift
+//  tryswiftconfTests
+//
+//  Created by Doan Truong Thi on 2/1/16.
+//
+//
+
+import XCTest
+@testable import tryswiftconf
+
+class tryswiftconfTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
